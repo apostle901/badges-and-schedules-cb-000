f688041f7cc199fe418403011d0aa08811033898
Jawann <jawann@gmail.com>
2017-11-27 01:57:35 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,3 +6,7 @@ end
 def batch_badge_creator (names)
   names.map { |e| badge_maker(e) }
 end
+
+def assign_rooms (names)
+  names.enum_with_index.map { |e, i| "Hello, #{e}! You'll be assigned to room #{i+1}!" }
+end
